99a337febf66e31b2119e4f42a35025f070c46c8
ppind <pearl.pind@gmail.com>
2024-11-23 11:41:21 +0200

    beautiful buttons for beautiful colors

---
+++ Assets/Scripts/ColorButton.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorButton : MonoBehaviour
+{
+    public GameObject walls;
+
+    private Image colorButtonImage;
+    void Start()
+    {
+        
+        colorButtonImage = GetComponent<Image>();
+
+        // Ensure this button has a Button component and add an onClick listener
+        Button button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(ChangeWallColors);
+        }
+        else
+        {
+            Debug.LogError("ColorButton must have a Button component!");
+        }
+    }
+
+    void ChangeWallColors()
+    {
+        if (walls == null)
+        {
+            Debug.LogError("Walls GameObject is not assigned!");
+            return;
+        }
+
+        Button[] wallButtons = walls.GetComponentsInChildren<Button>();
+
+        foreach (Button wallButton in wallButtons)
+        {
+            Image wallImage = wallButton.GetComponent<Image>();
+            if (wallImage != null)
+            {
+                wallImage.color = colorButtonImage.color;
+            }
+            else
+            {
+                Debug.LogWarning($"Button {wallButton.name} is missing an Image component.");
+            }
+        }
+    }
+}
