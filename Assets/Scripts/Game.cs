d3ee332b19e73279c7ba616a67073fad20b32a66
kzorlk <karlkristxfer@gmail.com>
2024-11-25 07:01:51 +0200

    added back references to components in unity

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -87,12 +87,12 @@ public class Game : MonoBehaviour
         string gameEndString = $"You achieved {happinessPercentage}% happiness.\n";
         if (happinessPercentage == 100) gameEndString += "WOW! The townsfolk couldn't be happier!";
         else if (happinessPercentage >= 75) gameEndString += "Impressive! The townsfolk are very happy!";
-        else if (happinessPercentage >= 50) gameEndString += "The townsfolk are pleased with the new development.";
-        else if (happinessPercentage >= 25) gameEndString += "The townsfolk are somewhat disappointed with your work";
-        else gameEndString += "The townsfolk are very displeased with their new properties.";
+        else if (happinessPercentage >= 50) gameEndString += "The townsfolk are pleased with their new residences.";
+        else if (happinessPercentage >= 25) gameEndString += "The townsfolk are disappointed.";
+        else gameEndString += "You should be ashamed.";
         GameEndText.text = gameEndString;
 
-        await(Task.Delay(300)); //MenuPanel doesn't open without this wait
+        await(Task.Delay(300)); //MenuPanel doesn't open without this
 
         SmallMenuPanel.SetActive(true);
         return;
